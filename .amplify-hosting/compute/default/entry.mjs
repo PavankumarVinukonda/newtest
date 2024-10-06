680bc77756dfd55877e10acaec049b9c7d772e3d
pavan kumar vinukonda <vinukondapavankumar1@gmail.com>
2024-10-07 00:44:44 +0530

    added

---
+++ .amplify-hosting/compute/default/entry.mjs
@@ -0,0 +1,32 @@
+import { renderers } from './renderers.mjs';
+import { s as serverEntrypointModule } from './chunks/_@astrojs-ssr-adapter_BJTfSXKX.mjs';
+import { manifest } from './manifest_DDWfQ8ND.mjs';
+
+const _page0 = () => import('./pages/_image.astro.mjs');
+const _page1 = () => import('./pages/index.astro.mjs');
+
+const pageMap = new Map([
+    ["node_modules/astro/dist/assets/endpoint/generic.js", _page0],
+    ["src/pages/index.astro", _page1]
+]);
+const serverIslandMap = new Map();
+const _manifest = Object.assign(manifest, {
+    pageMap,
+    serverIslandMap,
+    renderers,
+    middleware: () => import('./_noop-middleware.mjs')
+});
+const _args = {
+    "client": "file:///C:/Users/vinuk/Videos/Captures/New%20folder/interstellar-iron/.amplify-hosting/static/",
+    "server": "file:///C:/Users/vinuk/Videos/Captures/New%20folder/interstellar-iron/.amplify-hosting/compute/default/",
+    "host": false,
+    "port": 3000,
+    "assets": "_astro"
+};
+
+const _start = 'start';
+if (_start in serverEntrypointModule) {
+	serverEntrypointModule[_start](_manifest, _args);
+}
+
+export { pageMap };
--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,11 @@
 import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
 import awsAmplify from 'astro-aws-amplify';
- 
-import sitemap from '@astrojs/sitemap';
+import React from '@astrojs/react'
  
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
-  integrations: [mdx(), sitemap()],
+  integrations: [React()],
   adapter: awsAmplify(),
   output: 'server',
 });
